71b7d1fbe79c7b4a51e40c848995cf3fff92aa66
Simon Sanchez <simonsanchez.art@gmail.com>
2026-02-26 23:04:31 -0300

    feat: reset Panel Size on Separator Double-Click

--- src/components/CanvasSplitter.tsx
+++ src/components/CanvasSplitter.tsx
@@ -1,4 +1,4 @@
-import { Group as ResizableGroup, Panel, Separator, useDefaultLayout } from "react-resizable-panels";
+import { Group as ResizableGroup, Panel, Separator, useDefaultLayout, usePanelRef } from "react-resizable-panels";
 
 function CanvasSplitter({ className }: { className?: string }) {
     const PANEL_MIN_PERCENT = `${10}%`;
@@ -8,14 +8,25 @@ function CanvasSplitter({ className }: { className?: string }) {
         storage: localStorage,
     });
 
+    const leftPanel = usePanelRef();
+
     return (
         <div className={className}>
             <ResizableGroup defaultLayout={defaultLayout} onLayoutChanged={onLayoutChanged}>
-                <Panel id="markPanel" className="bg-red-500 rounded-l-md" minSize={PANEL_MIN_PERCENT}>
+                <Panel
+                    id="markPanel"
+                    panelRef={leftPanel}
+                    className="bg-red-500 rounded-l-md"
+                    minSize={PANEL_MIN_PERCENT}
+                >
                     Left
                 </Panel>
 
-                <Separator className="bg-amber-400 focus:outline-none w-0 opacity-100 drop-shadow-sm/50 data-[separator='hover']:w-2 data-[separator='active']:w-2 duration-100 transition-all" />
+                <Separator
+                    className="bg-amber-400 focus:outline-none w-0 opacity-100 drop-shadow-sm/50 hover:w-2 data-[separator='hover']:w-2 data-[separator='active']:w-2 duration-100 transition-all"
+                    onClick={(e) => e.preventDefault()}
+                    onDoubleClick={() => leftPanel.current?.resize("50%")}
+                />
 
                 <Panel id="atlasPanel" className="bg-blue-500 rounded-r-md" minSize={PANEL_MIN_PERCENT}>
                     Right
