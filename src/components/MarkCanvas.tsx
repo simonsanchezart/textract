3e4c54cf8b37b70a6be17cd88d96ffba22cbb4ec
Simon Sanchez <simonsanchez.art@gmail.com>
2026-03-02 22:18:14 -0300

    feat: work on MarkCanvas, Toolbar

--- src/components/MarkCanvas.tsx
+++ src/components/MarkCanvas.tsx
@@ -1,14 +1,12 @@
 import { ReactNode } from "react";
 import Canvas from "./Canvas";
-import { Rect } from "react-konva";
-import { CgAdd, CgClose, CgMaximizeAlt, CgMinimizeAlt } from "react-icons/cg";
+import { CgAdd } from "react-icons/cg";
 
-function MarkCanvas({ className }: { className?: string; chldren?: ReactNode }) {
+function MarkCanvas({ className = "" }: { className?: string; chldren?: ReactNode }) {
     return (
-        <div className="relative">
-            <Canvas className={className}></Canvas>
-            {/* toolbar: */}
-            <div className="flex text-center items-center absolute right-0 bg-dark-main-darker p-2 rounded-bl-2xl gap-2">
+        <div className="relative h-full" id="tester">
+            <Canvas className={`${className}`}></Canvas>
+            <div className="flex text-center items-center absolute bottom-0 left-0 bg-dark-main-darker p-2 rounded-tr-2xl gap-2">
                 <CgAdd className="size-6 button-icon" />
             </div>
         </div>
