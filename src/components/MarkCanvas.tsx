9a1e09d3caef6aa7607bcd7e30200425a2ba7b9f
Simon Sanchez <simonsanchez.art@gmail.com>
2026-03-04 22:58:36 -0300

    feat: work on Marks

--- src/components/MarkCanvas.tsx
+++ src/components/MarkCanvas.tsx
@@ -6,6 +6,8 @@ import { convertFileSrc } from "@tauri-apps/api/core";
 import useImage from "use-image";
 import { Stage, Layer, Rect, Image, Transformer, Circle, Line, KonvaNodeComponent, Group } from "react-konva";
 import Konva from "konva";
+import { KonvaEventObject } from "konva/lib/Node";
+import { KonvaPointerEvent } from "konva/lib/PointerEvents";
 
 //todo: test every image format
 
@@ -23,24 +25,92 @@ type MarkImageType = {
     src: string;
     x: number;
     y: number;
-    marks: MarkType[];
 };
 
 const MarkImageComponent = ({ imageData }: { imageData: MarkImageType }) => {
     const [image] = useImage(imageData.src);
+    const [marks, setMarks] = useState<MarkType[]>([]);
+    const [currentPoints, setCurrentPoints] = useState<PointType[]>([]);
+    const imageRef = useRef<Konva.Image | null>(null);
+    const POINT_SIZE = 4;
+
+    // marker: keep state with currentPoints
+    // once is reaches 4, push to MarkImageType marks
+    // allow for cancelling
+    // disable dragging, etc... while drawings
+
+    const addPoint = (e: KonvaPointerEvent) => {
+        const pos = imageRef.current?.getRelativePointerPosition();
+        if (pos == null) return;
+
+        setCurrentPoints((prev) => {
+            const updated = [...prev, { x: pos.x, y: pos.y }];
+
+            if (updated.length === 4) {
+                setMarks((marksPrev) => [...marksPrev, { points: updated }]);
+                return [];
+            }
+
+            return updated;
+        });
+    };
+
     return (
         <Group
             draggable
-            onDragStart={(e) => {
-                if (e.evt.buttons != 1) {
-                    e.target.stopDrag();
-                }
-            }}
+            x={imageData.x}
+            y={imageData.y}
+            onClick={addPoint}
+            className="bg-red-400"
+            // onMouseDown={(e) => {
+            //     if (e.evt.button !== 0) {
+            //         e.cancelBubble = false;
+            //         e.target.draggable(false);
+            //     } else {
+            //         e.target.draggable(true);
+            //     }
+            // }}
+            // onDragStart={(e) => {
+            //     if (e.evt.buttons != 1) {
+            //         e.target.stopDrag();
+            //     }
+            // }}
         >
-            <Image x={imageData.x} y={imageData.y} image={image} />
+            <Image ref={imageRef} image={image} />
 
-            {/* Draw Marks here */}
-            <Rect x={0} y={0} fill={"red"} width={32} height={32} draggable/>
+            {currentPoints.map((m) => (
+                <Rect
+                    x={m.x - POINT_SIZE / 2}
+                    y={m.y - POINT_SIZE / 2}
+                    fill={"red"}
+                    width={POINT_SIZE}
+                    height={POINT_SIZE}
+                    // draggable
+                />
+            ))}
+
+            {marks.map((m) => (
+                <Group draggable>
+                    {m.points.map((p) => (
+                        <Rect
+                            x={p.x - POINT_SIZE / 2}
+                            y={p.y - POINT_SIZE / 2}
+                            fill={"blue"}
+                            width={POINT_SIZE}
+                            height={POINT_SIZE}
+                            draggable
+                        />
+                    ))}
+
+                    <Line
+                        points={m.points.flatMap((p) => [p.x, p.y])}
+                        fill="#FF000022"
+                        stroke="white"
+                        strokeWidth={1}
+                        closed
+                    />
+                </Group>
+            ))}
         </Group>
     );
 };
@@ -75,6 +145,7 @@ function MarkCanvas({ className = "" }: { className?: string; chldren?: ReactNod
                             src: url,
                             x: 0,
                             y: 0,
+                            marks: [],
                         }));
                         setImages((prev) => [...prev, ...imageSet]);
                     }}
