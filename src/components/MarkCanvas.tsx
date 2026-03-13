f0591df3f9fdd23b89ef92bc4192812b01197646
Simon Sanchez <simonsanchez.art@gmail.com>
2026-03-12 21:12:02 -0300

    feat: improve UI/UX

--- src/components/MarkCanvas.tsx
+++ src/components/MarkCanvas.tsx
@@ -8,13 +8,9 @@ import { Rect, Image, Line, Group } from "react-konva";
 import Konva from "konva";
 import { KonvaEventObject } from "konva/lib/Node";
 import { Mark, MarkImage, Point2D, useMarkStore } from "../stores/markStore";
+import { Colors } from "../types/colors";
 
 //todo: test every image format
-
-//xconst: to internal config store
-const POINT_SIZE = 2;
-//xconst: to internal config store
-const POINT_SIZE_H = POINT_SIZE / 2;
 
 const getMiddle = (v2: Point2D[]) => {
     const average = v2.reduce((a, b) => {
@@ -30,22 +26,22 @@ const MarkComponent = ({ mark }: { mark: Mark }) => {
     const updateMark = useMarkStore((s) => s.updateMark);
     const updateMarkPoint = useMarkStore((s) => s.updateMarkPoint);
     const removeMark = useMarkStore((s) => s.removeMark);
+
     const [markOffset, setMarkOffset] = useState({ x: 0, y: 0 });
-    const [isHovered, setIsHovered] = useState(false);
 
     return (
         <Group draggable>
             <Line
                 points={mark.points.flatMap((p) => [p.x, p.y])}
-                fill={isHovered ? "#FF000022" : "#FFFFFF22"}
-                stroke="white"
-                strokeWidth={0.5}
+                fill={Colors.LIGHT + "22"}
+                stroke={Colors.LIGHT}
+                strokeWidth={1.0}
                 shadowOffset={{ x: 0.5, y: 0.5 }}
                 shadowOpacity={0.25}
                 closed
                 draggable
                 onClick={(e) => {
-                    removeMark(mark.id);
+                    if (e.evt.altKey) removeMark(mark.id);
                 }}
                 onDragMove={(e) => {
                     setMarkOffset({ x: e.target.x(), y: e.target.y() });
@@ -59,39 +55,49 @@ const MarkComponent = ({ mark }: { mark: Mark }) => {
                     e.target.setPosition({ x: 0, y: 0 });
                     setMarkOffset({ x: 0, y: 0 });
                 }}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
+                onPointerMove={(e) => {
+                    if (e.evt.altKey) {
+                        //xfunc: also cache?
+                        new Konva.Tween({
+                            node: e.target,
+                            duration: 0.02,
+                            fill: Colors.RED + "22",
+                        }).play();
+                    } else {
+                        //xfunc: also cache?
+                        new Konva.Tween({
+                            node: e.target,
+                            duration: 0.02,
+                            fill: Colors.BLUE + "22",
+                        }).play();
+                    }
+                }}
+                onPointerLeave={(e) => {
+                    //xfunc: also cache?
+                    new Konva.Tween({
+                        node: e.target,
+                        duration: 0.02,
+                        fill: Colors.LIGHT + "22",
+                    }).play();
+                }}
             />
 
-            <Rect offset={{ x: POINT_SIZE_H, y: POINT_SIZE_H }} fill={"green"} width={POINT_SIZE} height={POINT_SIZE} />
-
-            {/* xfunc: extract point drawing */}
-            {mark.points.map((p, i) => (
-                <Rect
-                    key={i}
-                    x={p.x}
-                    y={p.y}
-                    strokeWidth={0.5}
-                    fill={"#282828"}
-                    stroke={"white"}
-                    shadowOffset={{ x: 0.25, y: 0.25 }}
-                    shadowOpacity={0.25}
-                    cornerRadius={100}
-                    width={POINT_SIZE}
-                    height={POINT_SIZE}
-                    offset={{ x: POINT_SIZE_H - markOffset.x, y: POINT_SIZE_H - markOffset.y }}
-                    draggable
-                    onDragMove={(e) => {
-                        //todo: updating the actual point position should only be done on drag end
-                        // meanwhile this should just set an offset to display
-                        const newPoint = {
-                            x: e.target.x(),
-                            y: e.target.y(),
-                        };
-                        updateMarkPoint(mark.id, i, newPoint);
-                    }}
-                />
-            ))}
+            {mark.points.map((p, id) => {
+                return (
+                    <MarkPoint
+                        key={id}
+                        position={p}
+                        offset={markOffset}
+                        onDragMove={(e) => {
+                            const newPoint = {
+                                x: e.target.x(),
+                                y: e.target.y(),
+                            };
+                            updateMarkPoint(mark.id, id, newPoint);
+                        }}
+                    />
+                );
+            })}
         </Group>
     );
 };
@@ -147,28 +153,72 @@ const MarkImageComponent = ({ imageData }: { imageData: MarkImage }) => {
 
             {currentPoints.map((m, i) => (
                 <Group key={i}>
-                    <Rect
-                        x={m.x - POINT_SIZE_H}
-                        y={m.y - POINT_SIZE_H}
-                        fill={"red"}
-                        width={POINT_SIZE}
-                        height={POINT_SIZE}
-                        listening={false}
-                    />
+                    <MarkPoint position={{ x: m.x, y: m.y }} offset={{ x: 0, y: 0 }} />
 
                     <Line
                         points={currentPoints.flatMap((p) => [p.x, p.y])}
-                        fill="#FF000022"
-                        stroke="white"
+                        fill="#00FF0022"
+                        stroke={Colors.LIGHT}
                         strokeWidth={1}
                         closed
                         listening={false}
+                        dash={[2, 10]}
                     />
                 </Group>
             ))}
 
             {imageData.markIds.map((id) => marks[id] && <MarkComponent key={id} mark={marks[id]} />)}
         </Group>
+    );
+};
+
+type MarkPointProps = {
+    position: Point2D;
+    offset?: Point2D;
+} & React.ComponentProps<typeof Rect>;
+
+const MarkPoint = ({ position, offset = { x: 0, y: 0 }, ...props }: MarkPointProps) => {
+    const POINT_SIZE = 2;
+    const POINT_SIZE_H = POINT_SIZE / 2;
+
+    return (
+        <Rect
+            x={position.x}
+            y={position.y}
+            strokeWidth={0.5}
+            fill={"#282828"}
+            stroke={Colors.LIGHT}
+            shadowOffset={{ x: 0.25, y: 0.25 }}
+            shadowOpacity={0.25}
+            cornerRadius={100}
+            width={POINT_SIZE}
+            height={POINT_SIZE}
+            offset={{ x: POINT_SIZE_H - offset.x, y: POINT_SIZE_H - offset.y }}
+            onPointerEnter={(e) => {
+                //xfunc: also cache?
+                new Konva.Tween({
+                    node: e.target,
+                    duration: 0.02,
+                    width: POINT_SIZE * 1.5,
+                    height: POINT_SIZE * 1.5,
+                    offsetX: POINT_SIZE_H * 1.5 - offset.x,
+                    offsetY: POINT_SIZE_H * 1.5 - offset.y,
+                }).play();
+            }}
+            onPointerLeave={(e) => {
+                //xfunc: also cache?
+                new Konva.Tween({
+                    node: e.target,
+                    duration: 0.02,
+                    width: POINT_SIZE,
+                    height: POINT_SIZE,
+                    offsetX: POINT_SIZE_H - offset.x,
+                    offsetY: POINT_SIZE_H - offset.y,
+                }).play();
+            }}
+            draggable
+            {...props}
+        />
     );
 };
 
