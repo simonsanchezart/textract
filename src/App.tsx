8153e3926cd3e4f4e776efa0bca36bc36fa7a232
Simon Sanchez <simonsanchez.art@gmail.com>
2026-03-20 21:27:29 -0300

    refactor: remove demo alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Textract from "./components/Textract";
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
 
 function App() {
-
-
     return (
         <main className="flex flex-col h-screen text-light-main dark">
             <Navbar />
             <Textract />
-            <AlertDialog>
-                <AlertDialogTrigger asChild>
-                    <Button>Show Dialog</Button>
-                </AlertDialogTrigger>
-                <AlertDialogContent className="dark">
-                    <AlertDialogHeader>
-                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                        <AlertDialogDescription>
-                            This action cannot be undone. This will permanently delete your account from our servers.
-                        </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                        <AlertDialogCancel>Cancel</AlertDialogCancel>
-                        <AlertDialogAction>Continue</AlertDialogAction>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialog>
         </main>
     );
 }
