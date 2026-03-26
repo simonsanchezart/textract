f06a5c3d03f34faff8c366bbd4927b0de8334381
Simon Sanchez <simonsanchez.art@gmail.com>
2026-03-25 21:00:51 -0300

    refactor: add immer

--- src/stores/markStore.ts
+++ src/stores/markStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
 export type Point2DType = {
     x: number;
@@ -37,151 +38,77 @@ type MarkStore = {
 };
 
 export const useMarkStore = create(
-    persist<MarkStore>(
-        (set) => ({
+    persist(
+        immer<MarkStore>((set) => ({
             images: {},
             marks: {},
 
             addImage: (image) =>
-                set((state) => ({
-                    images: { ...state.images, [image.id]: image },
-                })),
-
-            updateImagePosition: (imageId: string, newPos: Point2DType) =>
                 set((state) => {
-                    const image = state.images[imageId];
-                    if (!image) return state;
-
-                    return {
-                        images: {
-                            ...state.images,
-                            [imageId]: {
-                                ...image,
-                                position: newPos,
-                            },
-                        },
-                    };
+                    state.images[image.id] = image;
                 }),
-            updateImageScale: (imageId: string, newScale: Point2DType) =>
+
+            updateImagePosition: (imageId, newPos) =>
                 set((state) => {
-                    const image = state.images[imageId];
-                    if (!image) return state;
-
-                    console.log(newScale)
-
-                    return {
-                        images: {
-                            ...state.images,
-                            [imageId]: {
-                                ...image,
-                                scale: newScale,
-                            },
-                        },
-                    };
+                    if (!state.images[imageId]) return;
+                    state.images[imageId].position = newPos;
                 }),
-            updateImageRotation: (imageId: string, newRot: number) =>
-                set((state) => {
-                    const image = state.images[imageId];
-                    if (!image) return state;
 
-                    return {
-                        images: {
-                            ...state.images,
-                            [imageId]: {
-                                ...image,
-                                rotation: newRot,
-                            },
-                        },
-                    };
+            updateImageScale: (imageId, newScale) =>
+                set((state) => {
+                    if (!state.images[imageId]) return;
+                    state.images[imageId].scale = newScale;
+                }),
+
+            updateImageRotation: (imageId, newRot) =>
+                set((state) => {
+                    if (!state.images[imageId]) return;
+                    state.images[imageId].rotation = newRot;
                 }),
 
             addMark: (imageId, mark) =>
                 set((state) => {
-                    const image = state.images[imageId];
-                    if (!image) return state;
-
-                    return {
-                        marks: {
-                            ...state.marks,
-                            [mark.id]: mark,
-                        },
-
-                        images: {
-                            ...state.images,
-                            [imageId]: {
-                                ...image,
-                                markIds: [...image.markIds, mark.id],
-                            },
-                        },
-                    };
+                    if (!state.images[imageId]) return;
+                    state.marks[mark.id] = mark;
+                    state.images[imageId].markIds.push(mark.id);
                 }),
+
             updateMark: (markId, newPoints) =>
                 set((state) => {
-                    const mark = state.marks[markId];
-                    if (!mark) return state;
-
-                    return {
-                        marks: {
-                            ...state.marks,
-                            [markId]: { ...mark, points: newPoints },
-                        },
-                    };
+                    if (!state.marks[markId]) return;
+                    state.marks[markId].points = newPoints;
                 }),
-            updateMarkPoint: (markId: string, pointIdx: number, newPoint: Point2DType) =>
+
+            updateMarkPoint: (markId, pointIdx, newPoint) =>
                 set((state) => {
-                    const mark = state.marks[markId];
-                    if (!mark) return state;
-
-                    const newPoints = [...mark.points];
-                    newPoints[pointIdx] = newPoint;
-
-                    return {
-                        marks: {
-                            ...state.marks,
-                            [markId]: { ...mark, points: newPoints },
-                        },
-                    };
+                    if (!state.marks[markId]) return;
+                    state.marks[markId].points[pointIdx] = newPoint;
                 }),
 
             removeMark: (markId) =>
                 set((state) => {
                     const mark = state.marks[markId];
-                    if (!mark) return state;
+                    if (!mark) return;
+                    if (!state.images[mark.imageId]) return;
 
-                    const image = state.images[mark.imageId];
-                    if (!image) return state;
-
-                    const { [markId]: _, ...remainingmarks } = state.marks;
-
-                    return {
-                        marks: remainingmarks,
-                        images: {
-                            ...state.images,
-                            [mark.imageId]: {
-                                ...image,
-                                markIds: image.markIds.filter((m) => m != markId),
-                            },
-                        },
-                    };
+                    delete state.marks[markId];
+                    state.images[mark.imageId].markIds = state.images[mark.imageId].markIds.filter(
+                        (id) => id !== markId
+                    );
                 }),
+
             removeImage: (imageId) =>
                 set((state) => {
                     const image = state.images[imageId];
-                    if (!image) return state;
+                    if (!image) return;
 
-                    const { [imageId]: _, ...remainingImages } = state.images;
-
-                    const remainingMarks = { ...state.marks };
                     for (const markId of image.markIds) {
-                        delete remainingMarks[markId];
+                        delete state.marks[markId];
                     }
 
-                    return {
-                        images: remainingImages,
-                        marks: remainingMarks,
-                    };
+                    delete state.images[imageId];
                 }),
-        }),
+        })),
         { name: "mark-storage" }
     )
 );
