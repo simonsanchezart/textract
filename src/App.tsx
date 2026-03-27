cce513d786c517d3394fd645f80e06feb6a376bc
Simon Sanchez <simonsanchez.art@gmail.com>
2026-03-27 20:04:15 -0300

    feat: disable refreshing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import Navbar from "./components/Navbar";
 import Textract from "./components/Textract";
 
 function App() {
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "F5" || (e.ctrlKey && e.key === "r") || (e.metaKey && e.key === "r")) {
+            e.preventDefault();
+        }
+    });
+
     return (
         <main className="flex flex-col h-screen text-light-main dark">
             <Navbar />
