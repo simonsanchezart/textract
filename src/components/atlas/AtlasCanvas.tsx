55f82703061c60cb91884776e8a409a94eb5257e
Simon Sanchez <simonsanchez.art@gmail.com>
2026-04-04 16:28:54 -0300

    feat: save transforms for Atlas Image

--- src/components/atlas/AtlasCanvas.tsx
+++ src/components/atlas/AtlasCanvas.tsx
@@ -3,9 +3,18 @@ import Canvas from "../Canvas";
 import { AtlasImageType, useAtlasStore } from "@/stores/atlasStore";
 import useImage from "use-image";
 import { Group, Image } from "react-konva";
+import Konva from "konva";
 
 function AtlasImageComponent({ imageData }: { imageData: AtlasImageType }) {
+    const updateImagePosition = useAtlasStore((s) => s.updateImagePosition);
+    const updateImageScale = useAtlasStore((s) => s.updateImageScale);
+    const updateImageRotation = useAtlasStore((s) => s.updateImageRotation);
     const [image] = useImage(imageData.base64);
+
+    const onDragEnd = (e: Konva.KonvaEventObject<DragEvent>) => {
+        if (e.target.name() !== "master") return;
+        updateImagePosition(imageData.id, { x: e.currentTarget.attrs.x, y: e.currentTarget.attrs.y });
+    };
 
     return (
         <Group
@@ -15,6 +24,24 @@ function AtlasImageComponent({ imageData }: { imageData: AtlasImageType }) {
             y={imageData.position.y}
             rotation={imageData.rotation}
             scale={imageData.scale}
+            onTransformEnd={(e) => {
+                const attrs = e.currentTarget.attrs;
+                const scale = { x: attrs.scaleX, y: attrs.scaleY };
+                const pos = { x: attrs.x, y: attrs.y };
+                const rotation = attrs.rotation;
+
+                updateImageScale(imageData.id, scale);
+                updateImageRotation(imageData.id, rotation);
+                updateImagePosition(imageData.id, pos);
+            }}
+            onDragStart={(e) => {
+                if (e.evt.buttons !== 1) {
+                    e.target.stopDrag();
+                    const stage = e.target.getStage();
+                    if (stage && e.evt.buttons === 4) stage.startDrag();
+                }
+            }}
+            onDragEnd={onDragEnd}
             draggable
         >
             <Image image={image} />
