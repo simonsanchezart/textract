df1184849163282ad81736f27c06193a931cecb5
Simon Sanchez <simonsanchez.art@gmail.com>
2026-04-20 23:10:54 -0300

    feat: move toolbar to upper left

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -12,7 +12,7 @@ export const ToolbarAction = ({ Icon, onClick, size = 6 }: ToolbarActionProps) =
 
 export const Toolbar = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="flex text-center items-center absolute bottom-0 left-0 bg-dark-main-darker p-2 rounded-tr-2xl gap-2">
+        <div className="flex text-center items-center absolute top-0 left-0 bg-dark-main-darker p-2 rounded-br-xl gap-2">
             {children}
         </div>
     );
