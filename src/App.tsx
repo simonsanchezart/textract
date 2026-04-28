be4b95b19c16e60051b2f881b6c2cbf3d4abb4df
Simon Sanchez <simonsanchez.art@gmail.com>
2026-04-27 22:58:47 -0300

    feat: add shadcn components
    Building blocks for Snapping value selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Navbar from "./components/Navbar";
 import Textract from "./components/Textract";
+import { Button } from "./components/ui/Button";
+import { ButtonGroup } from "./components/ui/button-group";
+import { Input } from "./components/ui/input";
 import "./App.css";
 
 function App() {
@@ -12,7 +15,21 @@ function App() {
   return (
     <main className="flex flex-col h-screen text-light-main dark">
       <Navbar />
+
       <Textract />
+
+      <div className="bg-dark-main-darker/80 p-2 flex gap-2">
+        <div className="select-none font-light tracking-widest">
+          Snapping
+        </div>
+
+        <ButtonGroup aria-label="Snapping Controls" className="h-fit">
+          <Button variant="outline" size="icon-xs">-</Button>
+          {/* <Input type="number" value={8} /> */}
+          <Button variant="outline" size="icon-xs">+</Button>
+        </ButtonGroup>
+      </div>
+
     </main>
   );
 }
