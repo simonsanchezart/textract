35e512a68f8f3b73201bab6ed1542b798d1832bb
Simon Sanchez <simonsanchez.art@gmail.com>
2026-04-28 20:22:48 -0300

    refactor: finish canvas hook extraction

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,14 +1,11 @@
 import type Konva from "konva";
-import type { Node, NodeConfig } from "konva/lib/Node";
-import type { KonvaPointerEvent } from "konva/lib/PointerEvents";
-import type { Box } from "konva/lib/shapes/Transformer";
 import type { ReactNode } from "react";
-import * as React from "react";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Layer, Shape, Stage, Transformer } from "react-konva";
 import useCanvasGrid from "@/hooks/use-canvas-grid";
+import useCanvasSelection from "@/hooks/use-canvas-selection";
+import useTransformSnapping from "@/hooks/use-canvas-snapping";
 import useCanvasZoom from "@/hooks/use-canvas-zoom";
-import { snap } from "@/lib/utils";
 import { useCanvasStore } from "@/stores/canvas-store";
 import { useSettingsStore } from "@/stores/settings-store";
 import { CanvasType } from "@/types/types";
@@ -23,87 +20,27 @@ type CanvasProps = {
 
 function Canvas({ canvasType, className, children, ...props }: CanvasProps) {
   const STAGE_SIZE = 2048;
-  const snapSize = useSettingsStore(s => s.snap);
 
-  const stageRef = useRef<Konva.Stage>(null);
+  const snapSize = useSettingsStore(s => s.snap);
   const canvasState = useCanvasStore(s => s.canvas[canvasType]);
 
-  const handleZoom = useCanvasZoom({ stageRef, canvasType });
-  const drawGrid = useCanvasGrid({ dotSize: 1, dotSpacing: snapSize }, stageRef);
-
+  const stageRef = useRef<Konva.Stage>(null);
   const transformerRef = useRef<Konva.Transformer | null>(null);
-  const [selectedNodes, setSelectedNodes] = useState<Node<NodeConfig>[]>([]);
 
+  const drawGrid = useCanvasGrid({ dotSize: 1, dotSpacing: snapSize }, stageRef);
+  const handleZoom = useCanvasZoom({ stageRef, canvasType });
+  const handleSelection = useCanvasSelection({ transformerRef });
+  const [handleDragMove, handleTransformSnapping] = useTransformSnapping({ stageRef, snapSize });
   const [openConfirmation, setOpenConfirmation] = useState(false);
 
   const handleShortcuts = async (e: React.KeyboardEvent<HTMLDivElement>) => {
     switch (e.code) {
-      case "Delete":{
+      case "Delete": {
         const selected = transformerRef.current?.nodes() ?? [];
         if (selected.length !== 0)
           setOpenConfirmation(true);
       }
     }
-  };
-
-  useEffect(() => {
-    transformerRef.current?.nodes(selectedNodes);
-  }, [selectedNodes]);
-
-  // refactor: extract to hook (with above useEffect)
-  const handleSelection = (e: KonvaPointerEvent) => {
-    if (e.target === e.target.getStage()) {
-      setSelectedNodes([]);
-      return;
-    }
-
-    const group = e.target.findAncestor(".master", false);
-    if (!group)
-      return;
-
-    setSelectedNodes((prev) => {
-      if (!e.evt.shiftKey)
-        return [group];
-      if (selectedNodes.includes(group))
-        return prev.filter(i => i !== group);
-      return [...prev, group];
-    });
-  };
-
-  // refactor: extract to hook
-  const handleDragMove = (e: Konva.KonvaEventObject<DragEvent>) => {
-    const target = e.target;
-    if (target.name() !== "master")
-      return;
-
-    const x = snap(target.x(), snapSize);
-    const y = snap(target.y(), snapSize);
-
-    target.position({ x, y });
-  };
-
-  // refactor: extract to hook (with handleDragMove)
-  const handleTransformSnapping = (_oldBox: Box, newBox: Box) => {
-    const stage = stageRef.current;
-    if (!stage)
-      return newBox;
-
-    const scale = stage.scaleX();
-    const stageX = stage.x();
-    const stageY = stage.y();
-
-    const newX = snap((newBox.x - stageX) / scale, snapSize);
-    const newY = snap((newBox.y - stageY) / scale, snapSize);
-    const newW = snap(newBox.width / scale, snapSize);
-    const newH = snap(newBox.height / scale, snapSize);
-
-    return {
-      x: newX * scale + stageX,
-      y: newY * scale + stageY,
-      width: Math.max(newW * scale, scale * snapSize),
-      height: Math.max(newH * scale, scale * snapSize),
-      rotation: newBox.rotation,
-    };
   };
 
   return (
