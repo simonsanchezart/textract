946d69d39920091e8c3a9a60a125a5482ee633d2
Simon Sanchez <simonsanchez.art@gmail.com>
2026-05-04 21:18:10 -0300

    chore: merge
    commit d3de9ffbd5448c54ca807e4be6f14e9652d28c8a
    Author: Simon Sanchez <simonsanchez.art@gmail.com>
    Date:   Mon May 4 21:17:15 2026 -0300
    
        perf: implement parallelism with Rayon and use warp_into

--- src-tauri/src/transformation.rs
+++ src-tauri/src/transformation.rs
@@ -1,6 +1,10 @@
 use base64::{engine::general_purpose, Engine};
-use image::{open, GenericImage, RgbaImage};
-use imageproc::geometric_transformations::{warp, Interpolation, Projection};
+use image::{open, RgbaImage};
+use imageproc::geometric_transformations::{warp_into, Interpolation, Projection};
+use rayon::{
+    iter::{IndexedParallelIterator, ParallelIterator},
+    slice::ParallelSlice,
+};
 use std::{io::Cursor, path::Path};
 
 fn get_quad_dimensions(points: &[(f32, f32); 4]) -> (f32, f32) {
@@ -13,49 +17,56 @@ fn get_quad_dimensions(points: &[(f32, f32); 4]) -> (f32, f32) {
 
 #[tauri::command]
 pub async fn transform_image(img_path: String, points: Vec<f32>) -> Result<Vec<String>, String> {
-    let mut buffers: Vec<String> = Vec::new();
-    let img = open(&img_path).unwrap().to_rgba8();
-    let img_name = Path::new(&img_path).file_name().and_then(|n| n.to_str()).unwrap_or("invalid");
-
-    let point_count = points.len();
-    let mark_count = point_count/8;
-
+    let mark_count = points.len() / 8 ;
     log::info!("Processing {mark_count} marks for {img_path}");
-    for i in (0..point_count).step_by(8) {
-        let tr = (points[i], points[i + 1]);
-        let tl = (points[i + 2], points[i + 3]);
-        let bl = (points[i + 4], points[i + 5]);
-        let br = (points[i + 6], points[i + 7]);
 
-        let src = [tl, tr, br, bl];
-        let (width, height) = get_quad_dimensions(&src);
-        let dst = [(0.0, 0.0), (width, 0.0), (width, height), (0.0, height)];
-        log::debug!("\n\tSource: {:?}\n\tDestination: {:?}", src, dst);
+    let img = open(&img_path).unwrap().to_rgba8();
+    let img_name = Path::new(&img_path)
+    .file_name()
+        .and_then(|n| n.to_str())
+        .unwrap_or("invalid");
 
-        let proj = Projection::from_control_points(src, dst).unwrap();
-        let mut result: RgbaImage = warp(
-            &img,
-            &proj,
-            Interpolation::Bilinear, // todo: pass interpolation as parameter
-            image::Rgba([0, 0, 0, 0]),
-        );
+    let buffers: Result<Vec<String>, String> = points
+        .par_chunks(8)
+        .enumerate()
+        .map(|(i, chunk)| {
+            let tr = (chunk[0], chunk[1]);
+            let tl = (chunk[2], chunk[3]);
+            let bl = (chunk[4], chunk[5]);
+            let br = (chunk[6], points[7]);
 
-        let crop_result = result.sub_image(0, 0, width as u32, height as u32);
-        let mut crop_buffer: Vec<u8> = Vec::new();
-        crop_result
-            .to_image()
-            .write_to(&mut Cursor::new(&mut crop_buffer), image::ImageFormat::Png)
-            .unwrap();
+            let src = [tl, tr, br, bl];
+            let (width, height) = get_quad_dimensions(&src);
+            let dst = [(0.0, 0.0), (width, 0.0), (width, height), (0.0, height)];
+            log::debug!("\n\tSource: {:?}\n\tDestination: {:?}", src, dst);
 
-        let base64 = format!(
-            "data:image/png;base64,{}",
-            general_purpose::STANDARD.encode(crop_buffer)
-        );
+            let proj = Projection::from_control_points(src, dst)
+                .ok_or_else(|| "Failed projection".to_string())?;
 
-        buffers.push(base64);
-        log::info!("Finished processing mark #{} for {}", ((i/8)+1), img_name);
-    }
+            let mut result = RgbaImage::new(width as u32, height as u32);
+            warp_into(
+                &img,
+                &proj,
+                Interpolation::Bilinear, // todo: pass interpolation as parameter
+                image::Rgba([0, 0, 0, 0]),
+                &mut result,
+            );
+
+            let mut crop_buffer: Vec<u8> = Vec::new();
+            result
+                .write_to(&mut Cursor::new(&mut crop_buffer), image::ImageFormat::Png)
+                .map_err(|e| e.to_string())?;
+
+            let base64 = format!(
+                "data:image/png;base64,{}",
+                general_purpose::STANDARD.encode(crop_buffer)
+            );
+
+            log::info!("Finished processing mark #{} for {}", (i + 1), img_name);
+            Ok(base64)
+        })
+        .collect();
 
     log::info!("Finished processing all marks for {}", img_name);
-    Ok(buffers)
+    buffers
 }
