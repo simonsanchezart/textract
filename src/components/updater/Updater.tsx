63686fd2819599de19a913f1807b8ef848925cdc
Simon Sanchez <simonsanchez.art@gmail.com>
2026-06-11 17:42:40 -0300

    feat: add update toast

--- src/components/updater/Updater.tsx
+++ src/components/updater/Updater.tsx
@@ -1,16 +1,34 @@
+import { getVersion } from "@tauri-apps/api/app";
+import { error } from "@tauri-apps/plugin-log";
+import { relaunch } from "@tauri-apps/plugin-process";
 import { check } from "@tauri-apps/plugin-updater";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 export function Updater() {
+  const [version, setVersion] = useState("0.0.0");
+
   useEffect(() => {
     const checkUpdate = async () => {
-      const update = await check();
-      console.log(update);
+      setVersion(await getVersion());
+
+      const update = await check().catch((err) => {
+        toast.error("Was not able to check for updates.");
+        error(err);
+      });
 
       if (!update)
         return;
 
-      console.log("Update found");
+      toast.info(`New Update Available`, {
+        action: {
+          label: "Install Update",
+          onClick: async () => {
+            await update?.downloadAndInstall();
+            await relaunch();
+          },
+        },
+      });
     };
 
     checkUpdate();
@@ -18,7 +36,7 @@ export function Updater() {
 
   return (
     <>
-      <h1>Hi</h1>
+      <h1>{version}</h1>
     </>
   );
 }
